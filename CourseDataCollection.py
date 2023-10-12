5b9b1d91a8998936b9deae4da8b40e6726519bbb
BrMaro <keremamariari@gmail.com>
2023-10-12 10:24:03 +0300

    can pull and save institution data

--- CourseDataCollection.py
+++ CourseDataCollection.py
@@ -1,14 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-import selenium.common.exceptions
-import time
-import pandas as pd
-import os
+
 import mysql.connector
 
 # Set up Google Chrome option and open browser
@@ -38,41 +32,41 @@ def login(kcse_index, year, password):
     driver.find_element(By.TAG_NAME, "button").click()
 
 
-def update_institution_values(id,alias,name, category, institution_type, parent_ministry):
+def update_institution_values(id, alias, name, category, institution_type, parent_ministry):
     update_institution_table_query = f"""
-    INSERT INTO Institution values(
-    {id} INT,
-    {alias} VARCHAR(255),
-    {name} VARCHAR(255),
-    {category} VARCHAR(255),
-    {institution_type} VARCHAR(255),
-    {parent_ministry} VARCHAR(255)
-    );
+    INSERT INTO Institution(id,alias,Name,Category,Institution_type,Parent_ministry)
+    VALUES(%s,%s,%s,%s,%s,%s)
     """
-    cursor.execute(update_institution_table_query)
-    cursor.fetchall()
+    data = (id, alias, name, category, institution_type, parent_ministry)
+    cursor.execute(update_institution_table_query, data)
+    conn.commit()
 
 
 def get_institution_data():
-    driver.find_element(By.XPATH, "(//ul[@class='nav in']//a)[2]").click() #Institution tab
-    select_element = driver.find_element(By.TAG_NAME,"select")
+    driver.find_element(By.XPATH, "(//ul[@class='nav in']//a)[2]").click()  # Institution tab
+    select_element = driver.find_element(By.TAG_NAME, "select")
     select = Select(select_element)
     select.select_by_index(3)
-    td_elements = driver.find_elements(By.CSS_SELECTOR, "td")
-    for i in range(0,100,6):
-        id = td_elements[i].text
-        alias = td_elements[i+1].text
-        name = td_elements[i+2].text
-        category = td_elements[i+3].text
-        institution_type = td_elements[i+4].text
-        parent_ministry = td_elements[i+5].text
-        #update_institution_values(id, alias, name, category, institution_type, parent_ministry)
-        print(f"{id}, {name} saved into table")
+    option_text = select.options[3].text
 
+    try:
+        while driver.find_element(By.LINK_TEXT, "Next").is_enabled():
+            td_elements = driver.find_elements(By.CSS_SELECTOR, "td")
+            for i in range(0, (int(option_text) * 6), 6):
+                id = td_elements[i].text
+                alias = td_elements[i + 1].text
+                name = td_elements[i + 2].text
+                category = td_elements[i + 3].text
+                institution_type = td_elements[i + 4].text
+                parent_ministry = td_elements[i + 5].text
+                update_institution_values(id, alias, name, category, institution_type, parent_ministry)
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.find_element(By.LINK_TEXT, "Next").click()
+    except IndexError:
+        pass
 
 
 login(index_no, examination_year, password)
-
 
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
@@ -87,6 +81,7 @@ id INT PRIMARY KEY,
 alias VARCHAR(255),
 Name VARCHAR(255),
 Category VARCHAR(255),
+institution_type VARCHAR(255),
 Parent_Ministry VARCHAR(255)
 );
 """
