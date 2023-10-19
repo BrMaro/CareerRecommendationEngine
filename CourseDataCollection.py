63fa059ecdae0b1edde862e07041679f49f5949d
BrMaro <keremamariari@gmail.com>
2023-10-19 10:50:08 +0300

    updating get_course_and_certification_data

--- CourseDataCollection.py
+++ CourseDataCollection.py
@@ -2,9 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from collections import Counter
 import mysql.connector
 
 # Set up Google Chrome option and open browser
@@ -44,26 +42,41 @@ def check_certification_duplicate_records(prog_code):
         return False
 
 
-def update_course_values(id, alias, course_name, Cluster, Cluster_s_1, Cluster_s_2, Cluster_s_3, Cluster_s_4, Min_s_1,
-                         Min_s_1_grade, Min_s_2, Min_s_2_grade, Min_subject_3, Min_s_3_grade, Overall_grade):
+def update_course_values(id, course_name, Cluster, Cluster_s_1, Cluster_s_2, Cluster_s_3, Cluster_s_4, Min_s_1,
+                         Min_s_1_grade, Min_s_2, Min_s_2_grade, Min_s_3, Min_s_3_grade, Min_s_4, Min_s_4_grade,Min_grade):
     update_course_table_query = f"""
-    INSERT INTO Institution(id,alias,course_name,Cluster,Cluster_subject_1 ,Cluster_subject_2,Cluster_subject_3 ,Cluster_subject_4 ,Min_subject_1,Min_subject_1_grade ,Min_subject_2 ,Min_subject_2_grade,Min_subject_3 ,Min_subject_3_grade,Overall_grade)
-    VALUES(%s,%s,%s,%s,%s,%s)
+    INSERT INTO Institution(id,course_name,Cluster,Cluster_subject_1 ,Cluster_subject_2,Cluster_subject_3 ,Cluster_subject_4 ,Min_subject_1,Min_subject_1_grade ,Min_subject_2 ,Min_subject_2_grade,Min_subject_3 ,Min_subject_3_grade,Min_subject_4 ,Min_subject_4_grade,Minimum_Mean_Grade)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
     """
-    data = (id, alias, course_name, Cluster, Cluster_s_1, Cluster_s_2, Cluster_s_3, Cluster_s_4, Min_s_1, Min_s_1_grade,
-            Min_s_2, Min_s_2_grade, Min_subject_3, Min_s_3_grade, Overall_grade)
+    data = (id, course_name, Cluster, Cluster_s_1, Cluster_s_2, Cluster_s_3, Cluster_s_4, Min_s_1, Min_s_1_grade,
+            Min_s_2, Min_s_2_grade, Min_s_3, Min_s_3_grade, Min_s_4, Min_s_4_grade,Min_grade)
     cursor.execute(update_course_table_query, data)
     conn.commit()
 
 
-def update_certification_values(Programme_Code,Iname,course_name,Programme_Name,Year_1_Programme_cost,_2022_cut_off,_2021_cut_off,_2020_cut_off):
+def update_certification_values(Programme_Code, Iname, course_name, Programme_Name, Year_1_Programme_cost,
+                                _2022_cut_off, _2021_cut_off, _2020_cut_off):
     update_certification_table_query = f"""
         INSERT INTO Institution(Programme_Code,Iname,course_name,Programme_Name,Year_1_Programme_cost,_2022_cut_off,_2021_cut_off,_2020_cut_off)
-        VALUES(%s,%s,%s,%s,%s,%s)
+        VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
         """
-    data = (Programme_Code,Iname,course_name,Programme_Name,Year_1_Programme_cost,_2022_cut_off,_2021_cut_off,_2020_cut_off)
+    data = (Programme_Code, Iname, course_name, Programme_Name, Year_1_Programme_cost, _2022_cut_off, _2021_cut_off,
+            _2020_cut_off)
     cursor.execute(update_certification_table_query, data)
     conn.commit()
+
+
+def most_common_code(numbers):
+    if not numbers:
+        return None  # Return None if the input list is empty.
+
+    # Use Counter to count the occurrences of each element.
+    count = Counter(numbers)
+
+    # Find the element with the highest count.
+    most_common = max(count, key=count.get)
+
+    return most_common
 
 
 def get_course_and_certfication_data():
@@ -84,16 +97,82 @@ def get_course_and_certfication_data():
         select = Select(select_element)
         select.select_by_index(3)
 
-        td_elements = driver.find_elements(By.CSS_SELECTOR, "td")
+        td2_elements = driver.find_elements(By.CSS_SELECTOR, "td")
 
-        for i in range(len(td_elements)):
-            td_elements[i].click()
+        for i in range(len(td2_elements)):
+            td2_elements[i].click()
             course_name = driver.find_element(By.XPATH, "//h3[@class='text-center']").text
             cluster = driver.find_element(By.XPATH, "//button[contains(@class,'btn btn-outline')]").text
             print(course_name, cluster)
 
-            table = driver.find_element(By.TAG_NAME, "table")
-            entry_requirements = driver.find_element(By.XPATH, "//div=[@class = 'col-md-6']")
+
+            Cluster_s_1 = ""
+            Cluster_s_2 = ""
+            Cluster_s_3 = ""
+            Cluster_s_4 = ""
+            Min_s_1 = ""
+            Min_s_1_grade = ""
+            Min_s_2 = ""
+            Min_s_2_grade = ""
+            Min_s_3 = ""
+            Min_s_3_grade = ""
+            Min_s_4 = ""
+            Min_s_4_grade = ""
+            Min_grade = ""
+            table_elements = driver.find_elements(By.XPATH, "//div[@class = 'col-md-6']")
+            # scrape from the minimum entry requirements table
+            entry_td3_elements = table_elements[0].find_elements(By.TAG_NAME, "td")
+            if len(entry_td3_elements) == 5:
+                Cluster_s_1 = entry_td3_elements[0].text
+                Cluster_s_2 = entry_td3_elements[1].text
+                Cluster_s_3 = entry_td3_elements[2].text
+                Cluster_s_4 = entry_td3_elements[3].text
+            elif len(entry_td3_elements) == 4:
+                Cluster_s_1 = entry_td3_elements[0].text
+                Cluster_s_2 = entry_td3_elements[1].text
+                Cluster_s_3 = entry_td3_elements[2].text
+            elif len(entry_td3_elements) == 3:
+                Cluster_s_1 = entry_td3_elements[0].text
+                Cluster_s_2 = entry_td3_elements[1].text
+            elif len(entry_td3_elements) == 2:
+                Cluster_s_1 = entry_td3_elements[0].text
+            elif len(entry_td3_elements) == 2 and table_elements[0].find_element(By.TAG_NAME,"th").text =="Minimum Mean Grade":
+                Min_grade = entry_td3_elements[0].text
+
+            # Scrape from the minimum subject requirements table
+            subject_td3_elements = table_elements[1].find_elements(By.TAG_NAME, "td")
+            if len(subject_td3_elements) == 8:
+                Min_s_1 = subject_td3_elements[0].text
+                Min_s_1_grade = subject_td3_elements[1].text
+                Min_s_2 = subject_td3_elements[2].text
+                Min_s_2_grade = subject_td3_elements[3].text
+                Min_s_3 = subject_td3_elements[4].text
+                Min_s_3_grade = subject_td3_elements[5].text
+                Min_s_4 = subject_td3_elements[6].text
+                Min_s_4_grade = subject_td3_elements[7].text
+                print(Min_s_1, Min_s_1_grade)
+            elif len(subject_td3_elements) == 6:
+                Min_s_1 = subject_td3_elements[0].text
+                Min_s_1_grade = subject_td3_elements[1].text
+                Min_s_2 = subject_td3_elements[2].text
+                Min_s_2_grade = subject_td3_elements[3].text
+                Min_s_3 = subject_td3_elements[4].text
+                Min_s_3_grade = subject_td3_elements[5].text
+                print(Min_s_1, Min_s_1_grade)
+            elif len(subject_td3_elements) == 4:
+                Min_s_1 = subject_td3_elements[0].text
+                Min_s_1_grade = subject_td3_elements[1].text
+                Min_s_2 = subject_td3_elements[2].text
+                Min_s_2_grade = subject_td3_elements[3].text
+                print(Min_s_1, Min_s_1_grade)
+            elif len(subject_td3_elements) == 2:
+                Min_s_1 = subject_td3_elements[0].text
+                Min_s_1_grade = subject_td3_elements[1].text
+                print(Min_s_1, Min_s_1_grade)
+
+            # update values to the database
+            #update_course_values(id, course_name, cluster, Cluster_s_1, Cluster_s_2, Cluster_s_3, Cluster_s_4, Min_s_1, Min_s_1_grade,Min_s_2, Min_s_2_grade, Min_s_3, Min_s_3_grade,Min_s_4,Min_s_4_grade,Min_grade)
+
 
         print(f"Cluster {i + 1} collected")
 
@@ -112,7 +191,6 @@ get_course_and_certfication_data()
 create_course_table_query = """
 CREATE TABLE IF NOT EXISTS Course (
 id INT PRIMARY KEY,
-alias VARCHAR(255),
 programme_name VARCHAR(255),
 Cluster VARCHAR(255),
 Cluster_subject_1 VARCHAR(255),
@@ -125,7 +203,9 @@ Minimum_subject_2 VARCHAR(255),
 Minimum_subject_2_grade VARCHAR(255),
 Minimum_subject_3 VARCHAR(255),
 Minimum_subject_3_grade VARCHAR(255),
-Overall_grade VARCHAR(255)
+Minimum_subject_4 VARCHAR(255),
+Minimum_subject_4_grade VARCHAR(255),
+Minimum_Mean_Grade VARCHAR(255)
 );
 """
 create_certification_table_query = """
