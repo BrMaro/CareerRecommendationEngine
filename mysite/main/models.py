eb94bc18fb39d0c7361689561e1889b34c9a6328
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-27 15:34:05 +0300

    added certifications per intitution

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Institution(models.Model):
     id = models.IntegerField(blank=True, null=True)
     alias = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    iname = models.CharField(db_column='IName', primary_key=True, max_length=255)  # Field name made lowercase.
-    category = models.CharField(db_column='Category', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    iname = models.CharField(db_column='IName', primary_key=True, max_length=255)  
+    category = models.CharField(db_column='Category', max_length=255, blank=True, null=True)  
     institution_type = models.CharField(max_length=255, blank=True, null=True)
-    parent_ministry = models.CharField(db_column='Parent_Ministry', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    parent_ministry = models.CharField(db_column='Parent_Ministry', max_length=255, blank=True, null=True)  
 
     class Meta:
         managed = False
@@ -18,20 +18,20 @@ class Institution(models.Model):
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
     programme_name = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    cluster = models.CharField(db_column='Cluster', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    cluster_subject_1 = models.CharField(db_column='Cluster_subject_1', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    cluster_subject_2 = models.CharField(db_column='Cluster_subject_2', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    cluster_subject_3 = models.CharField(db_column='Cluster_subject_3', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    cluster_subject_4 = models.CharField(db_column='Cluster_subject_4', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_1 = models.CharField(db_column='Minimum_subject_1', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_1_grade = models.CharField(db_column='Minimum_subject_1_grade', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_2 = models.CharField(db_column='Minimum_subject_2', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_2_grade = models.CharField(db_column='Minimum_subject_2_grade', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_3 = models.CharField(db_column='Minimum_subject_3', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_3_grade = models.CharField(db_column='Minimum_subject_3_grade', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_4 = models.CharField(db_column='Minimum_subject_4', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_subject_4_grade = models.CharField(db_column='Minimum_subject_4_grade', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    minimum_mean_grade = models.CharField(db_column='Minimum_Mean_Grade', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    cluster = models.CharField(db_column='Cluster', max_length=255, blank=True, null=True) 
+    cluster_subject_1 = models.CharField(db_column='Cluster_subject_1', max_length=255, blank=True, null=True) 
+    cluster_subject_2 = models.CharField(db_column='Cluster_subject_2', max_length=255, blank=True, null=True)
+    cluster_subject_3 = models.CharField(db_column='Cluster_subject_3', max_length=255, blank=True, null=True)
+    cluster_subject_4 = models.CharField(db_column='Cluster_subject_4', max_length=255, blank=True, null=True)
+    minimum_subject_1 = models.CharField(db_column='Minimum_subject_1', max_length=255, blank=True, null=True)
+    minimum_subject_1_grade = models.CharField(db_column='Minimum_subject_1_grade', max_length=255, blank=True, null=True) 
+    minimum_subject_2 = models.CharField(db_column='Minimum_subject_2', max_length=255, blank=True, null=True)
+    minimum_subject_2_grade = models.CharField(db_column='Minimum_subject_2_grade', max_length=255, blank=True, null=True)
+    minimum_subject_3 = models.CharField(db_column='Minimum_subject_3', max_length=255, blank=True, null=True)
+    minimum_subject_3_grade = models.CharField(db_column='Minimum_subject_3_grade', max_length=255, blank=True, null=True)
+    minimum_subject_4 = models.CharField(db_column='Minimum_subject_4', max_length=255, blank=True, null=True)
+    minimum_subject_4_grade = models.CharField(db_column='Minimum_subject_4_grade', max_length=255, blank=True, null=True)
+    minimum_mean_grade = models.CharField(db_column='Minimum_Mean_Grade', max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -40,13 +40,13 @@ class Course(models.Model):
         
 class Certification(models.Model):
     programme_code = models.CharField(primary_key=True, max_length=255)
-    iname = models.ForeignKey(Institution, models.DO_NOTHING, db_column='Iname', blank=True, null=True)  # Field name made lowercase.
-    programme_name = models.ForeignKey(Course, models.DO_NOTHING, db_column='Programme_name', to_field='programme_name', blank=True, null=True)  # Field name made lowercase.
-    programme_name_in_campus = models.CharField(db_column='Programme_Name_in_campus', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    year_1_programme_cost = models.CharField(db_column='Year_1_Programme_cost', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    field_2022_cut_off = models.CharField(db_column='_2022_cut_off', max_length=255, blank=True, null=True)  # Field renamed because it started with '_'.
-    field_2021_cut_off = models.CharField(db_column='_2021_cut_off', max_length=255, blank=True, null=True)  # Field renamed because it started with '_'.
-    field_2020_cut_off = models.CharField(db_column='_2020_cut_off', max_length=255, blank=True, null=True)  # Field renamed because it started with '_'.
+    iname = models.ForeignKey(Institution, models.DO_NOTHING, db_column='Iname', blank=True, null=True)
+    programme_name = models.ForeignKey(Course, models.DO_NOTHING, db_column='Programme_name', to_field='programme_name', blank=True, null=True)
+    programme_name_in_campus = models.CharField(db_column='Programme_Name_in_campus', max_length=255, blank=True, null=True)
+    year_1_programme_cost = models.CharField(db_column='Year_1_Programme_cost', max_length=255, blank=True, null=True)
+    field_2022_cut_off = models.CharField(db_column='_2022_cut_off', max_length=255, blank=True, null=True)  
+    field_2021_cut_off = models.CharField(db_column='_2021_cut_off', max_length=255, blank=True, null=True)  
+    field_2020_cut_off = models.CharField(db_column='_2020_cut_off', max_length=255, blank=True, null=True)  
 
     class Meta:
         managed = False
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -34,7 +34,7 @@ def institution(response):
 
 def certifications_by_programme(response,programme_name):
     course = get_object_or_404(Course, programme_name=programme_name)
-    print(course)
+
     certifications = Certification.objects.filter(programme_name=course)
 
     if not certifications:
@@ -54,8 +54,29 @@ def certifications_by_programme(response,programme_name):
     return render(response, 'main/certifications_by_programme.html', {'certifications_data': certifications_data, 'programme_name': programme_name})
 
 
-def courses_by_institution():
-    pass
+def certifications_per_institution(response, iname):
+    institution = get_object_or_404(Institution, iname=iname)
+    print(institution)
+    certifications = Certification.objects.filter(iname=institution)
+    
+    if not certifications:
+        return render(response,"main/no_certifications_error.html",{'certifications': []})
+
+    certifications_data = []
+    for certification in certifications:
+        institution = certification.iname   #extract related Institution object     
+        
+        certification_data = {
+            'programme_code': certification.programme_code,
+            'category': institution.category,
+            'iname': institution.iname,
+            'year_1_programme_cost':certification.year_1_programme_cost
+        }
+        certifications_data.append(certification_data)
+    return render(response, 'main/certifications_per_institution.html',{'certifications_data': certifications_data, 'iname': iname})
+
+    
+    
 
 def courses_by_cluster():
     pass
